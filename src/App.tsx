24f4ddec880e7680fe672d4e138cf36aabbd6d39
laremund <larsgremund@gmail.com>
2025-12-18 17:10:10 -0700

    build(src): adjust to more standard file structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+      <p>React is now enabled! 🎉</p>
+    </div>
+  );
+}
+
+export default App;
